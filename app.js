666df79e5072bcc77635e4a853d5cf5a6f0c7e93
Giulio Mario Martena <giuliomariomartena@gmail.com>
2021-09-15 22:50:03 +0200

    Aggiunta connessione al DB

--- app.js
+++ app.js
@@ -1,15 +1,44 @@
-const express = require('express')
+const express = require("express");
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
 
+// Instantiate application
 const app = express();
 
+// Database
+const sequelize = new Sequelize(
+  process.env.DB_DATABASE,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+  }
+);
+const testDB = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection estabilished successfully");
+  } catch (err) {
+    console.error("Couldn't connect to database:", err.message);
+  }
+};
+
 // Middleware
-app.use(express.json())
+app.use(express.json());
 
 // Routing
-app.get('/', (req,res) => {res.sendStatus(200)})
-app.post('/', (req,res) => {
-    console.log(req.body)
-    res.json(req.body)
-})
+app.get("/", (req, res) => {
+  res.sendStatus(200);
+});
+app.post("/", (req, res) => {
+  console.log(req.body);
+  res.json(req.body);
+});
 
-app.listen(5000, () => console.log("Server listening on port 5000"))
+// Run server
+const PORT = process.env.PORT || 8081;
+app.listen(PORT, () => {
+  testDB();
+  console.log(`Server listening on port ${PORT}`);
+});
