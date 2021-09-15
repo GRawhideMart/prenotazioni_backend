0adff920411ee6a9f67b234a46fe150baa3d3fe9
Giulio Mario Martena <giuliomariomartena@gmail.com>
2021-09-15 23:54:43 +0200

    Modello per inventario

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require("express");
 
 // Database
 const db = require("./database/dbConfig");
+const Inventary = require("./models/Inventary");
+(async () => {
+  await db.sync();
+  console.log("Database synched correctly");
+})();
 
 // Instantiate application
 const app = express();
@@ -13,21 +18,27 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.sendStatus(200);
 });
-app.post("/", (req, res) => {
-  console.log(req.body);
-  res.json(req.body);
+app.post("/inventario", (req, res) => {
+  const item = Inventary.create(req.body)
+    .then((item) => {
+      console.log("Entry created");
+      res.json(item);
+    })
+    .catch((err) => console.error(err.message));
 });
 
 // Run server
 const PORT = process.env.PORT || 8081;
-
-db.authenticate()
+db.authenticate() // Try to connect to database
   .then(() => {
+    // Upon successful promise completion
     console.log("Connection estabilished successfully");
     app.listen(PORT, () => {
+      // Start server
       console.log(`Server listening on port ${PORT}`);
     });
   })
   .catch((err) => {
+    // Upon unfulfilled promise, DO NOT start server
     console.error("Couldn't connect to database:", err.message);
   });
---
+++ models/Inventary.js
@@ -0,0 +1,45 @@
+const { DataTypes } = require("sequelize");
+const db = require("../database/dbConfig");
+
+const Inventary = db.define(
+  "Inventario",
+  {
+    // Model attributes are defined here
+    quantita: {
+      type: DataTypes.INTEGER,
+      defaultValue: "1",
+    },
+    nome: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+    },
+    descrizione: {
+      type: DataTypes.STRING(255),
+    },
+    anno_acquisto: {
+      type: DataTypes.STRING(10),
+    },
+    prezzo: {
+      type: DataTypes.DECIMAL(6, 2),
+    },
+    donazione: {
+      type: DataTypes.INTEGER,
+      defaultValue: "0",
+    },
+    disponibile: {
+      type: DataTypes.INTEGER,
+      defaultValue: "0",
+    },
+    priorita_sostituzione: {
+      type: DataTypes.ENUM("BASSO", "MEDIO", "ALTO"),
+      allowNull: false,
+    },
+  },
+  {
+    tableName: "inventario",
+    timestamps: false,
+    engine: "MyISAM",
+  }
+);
+
+module.exports = Inventary;
