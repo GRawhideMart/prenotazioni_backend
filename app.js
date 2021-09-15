4fc60e681c8404a15bb2cc4da93fa7d90194029b
Giulio Mario Martena <giuliomariomartena@gmail.com>
2021-09-15 19:53:51 +0200

    Added README

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const express = require('express')
 
 const app = express();
 
+// Middleware
+app.use(express.json())
+
+// Routing
 app.get('/', (req,res) => {res.sendStatus(200)})
+app.post('/', (req,res) => {
+    console.log(req.body)
+    res.json(req.body)
+})
 
 app.listen(5000, () => console.log("Server listening on port 5000"))
